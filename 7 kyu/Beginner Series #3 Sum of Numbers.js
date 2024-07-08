f8a18b8e85ad926103797793027b211fe1037be7
maxim <trifonov.max@mail.ru>
2024-07-08 14:22:48 +0300

    add Beginner Series #3 Sum of Numbers

---
+++ 7 kyu/Beginner Series #3 Sum of Numbers.js
@@ -0,0 +1,17 @@
+function getSum(a, b) {
+  if (a == b) {
+    return a;
+  }
+  let sum = 0;
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+  }
+  if (a > b) {
+    for (let i = b; i <= a; i++) {
+      sum += i;
+    }
+  }
+  return sum;
+}
