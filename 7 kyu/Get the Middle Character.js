397df5817e488afbf295606dfea7257fdf349c5f
maxim <trifonov.max@mail.ru>
2024-07-08 10:13:01 +0300

    add Get the Middle Character

---
+++ 7 kyu/Get the Middle Character.js
@@ -0,0 +1,11 @@
+function getMiddle(s) {
+  if (s.length < 2) {
+    return s;
+  }
+
+  if (s.length % 2) {
+    return s[(s.length + 1) / 2 - 1];
+  }
+
+  return s[s.length / 2 - 1] + s[s.length / 2];
+}
