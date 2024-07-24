73f3c2d39b390ae780561efc9ab6ce07e60c6b05
maxim <trifonov.max@mail.ru>
2024-07-24 14:15:32 +0300

    add Chuck Norris VII - True or False? (Beginner)

---
+++ 8 kyu/chuck.js
@@ -0,0 +1,7 @@
+// Your task is to be more like Chuck (ha! good luck!). You must return false without ever actually using the word false...
+
+// Go show some truth who's boss!
+
+function ifChuckSaysSo() {
+  return !!0;
+}
